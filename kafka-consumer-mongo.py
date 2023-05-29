80f23417f21a8c4fb0ea193affe3c96d954c8f68
Anahi Ximena Sanchez Vasquez <106393069+XMvasquez@users.noreply.github.com>
2023-05-29 13:24:18 -0600

    Update kafka-consumer-mongo.py

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -37,13 +37,43 @@ consumer = KafkaConsumer('test',bootstrap_servers=['my-kafka-0.my-kafka-headless
 for msg in consumer:
     record = json.loads(msg.value)
     print(record)
-    name = record['name']
+    userid = record['userid']
+    objectid = record['objectid']
+    message = record['message']
+    print("hola :)")
+
 
     # Create dictionary and ingest data into MongoDB
     try:
-       tkdapp_rec = {'name':name }
-       print (tkdapp_rec)
-       tkdapp_id = db.tkdapp_info.insert_one(tkdapp_rec)
-       print("Data inserted with record ids", tkdapp_id)
-    except:
-       print("Could not insert into MongoDB")
+        tkdapp_rec = {'userid': userid, 'objectid': objectid, 'message': message}
+        print(tkdapp_rec)
+        tkdapp_id = db.tkdapp_comments.insert_one(tkdapp_rec)
+        print("Data inserted with record ids", tkdapp_id)
+
+        subprocess.call(['sh', './test.sh'])
+    except Exception as e:
+        print("Could not insert into MongoDB")
+        print(e)
+
+    # Create bdnosql_sumary and insert groups into mongodb
+    try:
+        agg_result = db.tkdapp_comments.aggregate([
+        {
+            "$group": {
+                "_id": {
+                    'objectid': '$objectid',
+                    'message': '$message'
+                },
+                "n": {"$sum": 1}
+            }
+        }
+    ])
+        db.tkdapp_comments_sumary.delete_many({})
+        for i in agg_result:
+            print(i)
+            sumary_id = db.tkdapp_comments_sumary.insert_one(i)
+            print("Sumary Reactions inserted with record ids: ", sumary_id)
+            
+    except Exception as e:
+        print(f'group vy cought {type(e)}: ')
+        print(e)
